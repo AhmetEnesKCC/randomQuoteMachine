f1b1ee1faac55d0b83f88dbcc0c4fa95f508805f
Ahmet Enes KCC <ahmetkcc2001@gmail.com>
2020-07-28 19:10:54 +0300

    v1.0

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import { reducers } from "./redux";
+import thunk from "redux-thunk";
+const middleWare = [thunk];
+
+const store = createStore(
+  reducers,
+  compose(
+    applyMiddleware(...middleWare),
+    (window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()) ||
+      function (f) {
+        return f;
+      }
+  )
+);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
