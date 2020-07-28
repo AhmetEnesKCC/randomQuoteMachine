688901776d6e401774fcce8b2911c29a9f3ace37
Ahmet Enes KCC <ahmetkcc2001@gmail.com>
2020-07-28 19:13:00 +0300

    Updates

---
+++ precache-manifest.7c3959c9659752547e0f2d58269a21c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130c7eb964784b76c445221d9c6abe0b",
+    "url": "/randomQuoteMachine/index.html"
+  },
+  {
+    "revision": "fd13c578ed7e000bb23f",
+    "url": "/randomQuoteMachine/static/css/main.7ffef877.chunk.css"
+  },
+  {
+    "revision": "aa31f78fe4e9003a733d",
+    "url": "/randomQuoteMachine/static/js/2.5e1e37c3.chunk.js"
+  },
+  {
+    "revision": "46f91ec18259a53c6f750cd89192fcc8",
+    "url": "/randomQuoteMachine/static/js/2.5e1e37c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd13c578ed7e000bb23f",
+    "url": "/randomQuoteMachine/static/js/main.33765474.chunk.js"
+  },
+  {
+    "revision": "a2c81a982c66b79dc18a",
+    "url": "/randomQuoteMachine/static/js/runtime-main.554418ac.js"
+  }
+]);
